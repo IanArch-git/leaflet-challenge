baab6b0f5e4861c599204cefa3a0efa4708bdb25
Ian Arch <ian.arch01@gmail.com>
2021-05-24 08:31:31 -0600

    URL request

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,2 +1,6 @@
 var usgsURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-console.log(usgsURL);
+// console.log(usgsURL);
+
+d3.json(usgsURL).then(function(data) {
+    createFeatures(data.features);
+});
