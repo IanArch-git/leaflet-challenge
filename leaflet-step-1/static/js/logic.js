bc8f9347999151f53475fcbd0c289cc0e67194ac
Ian Arch <ian.arch01@gmail.com>
2021-05-24 08:34:26 -0600

    feature popups and circles

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -4,3 +4,17 @@ var usgsURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_mon
 d3.json(usgsURL).then(function(data) {
     createFeatures(data.features);
 });
+
+function createFeatures(quakeData) {
+    function onEachFeature(feature,layer) {
+        layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+    }
+    function pointFunction(feature,layer) {
+        return L.circleMarker(layer, { radius: feature.properties.mag * 2});
+    }
+    var quakes = L.geoJSON(quakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: pointFunction
+    });
+    createMap(quakes);
+}
