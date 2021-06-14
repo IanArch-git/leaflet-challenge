e6d8cc2962194ddefda69049a917dd976ecdffd5
Ian Arch <ian.arch01@gmail.com>
2021-06-14 08:58:47 -0600

    API key change

--- static/js/config.js
+++ static/js/config.js
@@ -1,2 +1,2 @@
 // API key
-const API_KEY = "pk.eyJ1IjoiZXdlYmVyc21zIiwiYSI6ImNrb3RhMThiNzAwZDIyeG1wYXNkeWU2cTcifQ.Ci2kUuKs-8QYSaO1JaKoGA";
+const API_KEY = "pk.eyJ1IjoiYmVuamFtZW5hbGZvcmQiLCJhIjoiY2p2NWg5enJkMHVyMzQ0bWtmY2FtNmM5bCJ9.mFsGlikCvtB_-6xIFCVPiQ";
