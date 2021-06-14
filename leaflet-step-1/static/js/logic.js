2708a4623ae28a7f2ed6437fd18d2756136a358b
Ian Arch <ian.arch01@gmail.com>
2021-06-14 08:10:04 -0600

    Updates

--- leaflet-step-1/static/js/logic.js
+++ leaflet-step-1/static/js/logic.js
@@ -1,16 +1,16 @@
 var usgsURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.geojson";
-// console.log(usgsURL);
+console.log(usgsURL);
 
-d3.json(usgsURL).then(function(data) {
+d3.json(usgsURL).then(function (data) {
     createFeatures(data.features);
 });
 
 function createFeatures(quakeData) {
-    function onEachFeature(feature,layer) {
+    function onEachFeature(feature, layer) {
         layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
     }
-    function pointFunction(feature,layer) {
-        return L.circleMarker(layer, { radius: feature.properties.mag * 2});
+    function pointFunction(feature, layer) {
+        return L.circleMarker(layer, { radius: feature.properties.mag * 2 });
     }
     var quakes = L.geoJSON(quakeData, {
         onEachFeature: onEachFeature,
@@ -28,4 +28,36 @@ function createMap(quakes) {
         id: "mapbox/streets-v11",
         accessToken: API_KEY
     });
-}
+    var light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id:"light-v10",
+        accessToken: API_KEY
+    });
+    var dark = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "dark-v10",
+        accessToken: API_KEY
+    });
+    var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?" + "access_token=pk.eyJ1IjoidHNsaW5kbmVyIiwiYSI6ImNqaWNhdTFzdzFuam4za21sc3ZiMmN5bDEifQ.5Il8Y1QtwyMFWCa1JkDY_Q");
+    var myMap = L.map("mapid", {
+        center: [
+            37.09, -95.71
+        ],
+        zoom: 5,
+        layers: [streetMap, quakes]
+    });
+    var baseMaps = {
+        "Street Map": streetMap,
+        "Dark": dark,
+        "Light": light,
+        "Satellite": satellite
+    };
+    var overlayMaps = {
+        Earthquakes: quakes
+    };
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap);
+};
